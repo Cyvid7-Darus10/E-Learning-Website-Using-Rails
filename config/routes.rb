972dbddad72c79546bfdfc4547ce4c65e865c135
Cyrus David Pastelero <61748886+Cyvid7-Darus10@users.noreply.github.com>
2022-04-13 20:09:55 +0800

    [Layout] Sign Up (#2)
    * [Layout] Static Pages
    
    * Changed the identation to 4 spaces in application.html.erb
    
    * [Layout] Sign Up
    
    * Changed the route path and fixed identation

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
   root 'pages#home'
-
+  get 'signup',   to: 'users#new'
   get 'about',    to: 'pages#about'
   get 'contact',  to: 'pages#contact'
+
+  resources :users
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
