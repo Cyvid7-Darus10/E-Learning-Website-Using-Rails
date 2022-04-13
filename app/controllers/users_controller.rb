d4a897a10b8458c3b7637f7258ed85f9947ec496
Cyrus David Pastelero <cgpastelero@up.edu.ph>
2022-04-13 20:33:22 +0800

    [Function] Sign Up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,8 +7,21 @@ class UsersController < ApplicationController
   end
 
   def create
+    @user = User.new(user_params)
+    if @user.save
+      session[:user_id] = @user.id
+      flash[:success] = "Welcome, #{@user.name}!"
+      redirect_to root_path
+    else
+      render :new
+    end
   end
 
   def show
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
 end
