cc73acd86d3321574657d1c74e755613895d1829
Cyrus David Pastelero <61748886+Cyvid7-Darus10@users.noreply.github.com>
2022-04-18 19:29:05 +0800

    [Function] Authentication (#5)
    * [Function] Authentication
    
    * Updated the spacing for controller and helper from 4 spaces to 2 spaces.

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
   root 'pages#home'
+  
   get 'signup',   to: 'users#new'
   get 'about',    to: 'pages#about'
   get 'contact',  to: 'pages#contact'
   get 'login',    to: 'sessions#new'
+  get 'logout',   to: 'sessions#destroy'
 
   resources :users
   resources :sessions, only: :create
