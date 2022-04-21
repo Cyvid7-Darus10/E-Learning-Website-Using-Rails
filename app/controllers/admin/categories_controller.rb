a684ebeb642e54258d0bdd5c5b71c74367c5c6e6
Cyrus David Pastelero <cgpastelero@up.edu.ph>
2022-04-21 20:38:22 +0800

    [Function][Admin] Create Category

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -1,4 +1,6 @@
 class Admin::CategoriesController < ApplicationController
+  before_action :correct_user
+
   def index
     @categories = Category.paginate(page: params[:page], per_page: 9).order(created_at: :desc)
   end
@@ -7,7 +9,29 @@ class Admin::CategoriesController < ApplicationController
     @category = Category.new
   end
 
+  def create
+    @category = Category.new(category_params)
+    if @category.save
+      flash[:success] = "Category was successfully created"
+      redirect_to admin_categories_path
+    else
+      render "new"
+    end
+  end
+
   def edit
     @category = Category.find(params[:id])
+  end
+
+  private
+  def category_params
+    params.require(:category).permit(:title, :description)
+  end
+
+  def correct_user
+    if !current_user || !current_user.is_admin?
+      flash[:danger] = "You are not authorized to perform this action."
+      redirect_to root_path
+    end
   end
 end
--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,6 @@
 class Category < ApplicationRecord
+  validates :title, presence: true
+  validates :description, presence: true
   has_many :words, dependent: :destroy
-  has_many :lessons, depedent: :destroy
+  has_many :lessons, dependent: :destroy
 end
