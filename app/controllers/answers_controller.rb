a0905a3a632f14d07a7717b8640b377b365e10de
Cyrus David Pastelero <61748886+Cyvid7-Darus10@users.noreply.github.com>
2022-04-27 21:40:33 +0800

    [Function] Lesson Answering (#23)
    * [Function] Lesson Answering
    
    * simplified the code

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -1,6 +1,33 @@
 class AnswersController < ApplicationController
   def new
-    @category = Category.first
-    @answer = Answer.new
+    @lesson = Lesson.find_by_id(params[:lesson_id])
+    @category = @lesson.category
+
+    if @lesson.next_word.nil?
+      flash[:success] = "Congratulations! You have learned all words in #{@category.title}." 
+
+      @lesson.update(score: @lesson.get_score)
+      redirect_to lesson_path(@lesson)
+    else
+      @answer = Answer.new
+      @word = @lesson.next_word
+    end
+  end
+
+  def create
+    @lesson = Lesson.find_by_id(params[:lesson_id])
+    @answer = @lesson.answers.new(answer_params)
+    if @answer.save
+      flash[:success] = "Your answer has been saved."
+      redirect_to new_lesson_answer_path(@lesson)
+    else
+      flash[:danger] = "Your answer could not be saved."
+      redirect_to new_lesson_answer_path(@lesson)
+    end
+  end 
+
+  private
+  def answer_params
+    params.require(:answer).permit(:word_id, :choice_id)
   end
 end
